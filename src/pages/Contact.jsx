129623c1f5a0d34daa44342f0f1e225e1228c165
mskchaithanyaraj <mskchaithanyaraj@gmail.com>
2025-06-02 18:43:57 +0530

    updates

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Contact = () => (
   <div>
     <h1 className="text-3xl font-bold mb-4">Contact Page</h1>
-    <p>This is the Chaithanya :)</p>
+    <p>mskchaithanyaraj@gmail.com</p>
   </div>
 );
 
